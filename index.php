1e699e18613eb74d7c9f0d2413ff75186e878a1f
unknown <kwameagyenimboateng64@gmail.com>
2020-03-24 12:49:55 +0000

    checks connection failure and success

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+    include 'db_connection.php';
+    
+    $conn = OpenConnection();
+
+    //query goes here
+
+    echo " Connected Successfully";
+
+    CloseConnection($conn); 
+
+
+
+
+
+//
+//black man doesnt like listening to ideas
+//  uber owns no car but employs people with cars who sweat to make money and take money from them.
+
+
+
+?>
+
